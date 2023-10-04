d28a571a6a28b922c61b414f357c060325552681
Yorlin Arias <yorlinandrey@gmail.com>
2023-10-03 22:05:27 -0500

    Se crearon los atributos en el archivo article

---
+++ src/main/java/com/sistema/inventario/model/Article.java
@@ -0,0 +1,20 @@
+package com.sistema.inventario.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Article {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String price;
+    private String description;
+    private String color;
+    private String material;
+    private String quantity;
+}
