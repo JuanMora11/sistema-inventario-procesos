b03e8ee9b72e38b74fb60e47c8544adf00728169
Yorlin Arias <yorlinandrey@gmail.com>
2023-10-03 22:24:34 -0500

    Se modificó el nombre de los archivos y se agregaron los metodos correspondientes a cada archivo

---
+++ src/main/java/com/sistema/inventario/repository/ArticleRepository.java
@@ -0,0 +1,10 @@
+package com.sistema.inventario.repository;
+
+import com.sistema.inventario.model.Article;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface ArticleRepository extends CrudRepository<Article, Long> {
+    List<Article> findByName(String name);
+}
